39bb003f2a04c89d707f2dfadc6d4c0d6b659b12
Kyrylo Silin <silin@kyrylo.org>
2024-06-14 20:06:02 +0300

    Make build green

--- test/telebugs/test_rails.rb
+++ test/telebugs/test_rails.rb
@@ -6,8 +6,4 @@ class Telebugs::TestRails < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Telebugs::Rails::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
